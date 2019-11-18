7f71cf46a26e1e419510fbadd66cab33a2643772
Jesse Gonzalez <jessegonzalez024@gmail.com>
2019-11-18 22:31:22 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -10,7 +10,7 @@ class CashRegister
   def add_item (title, price, quantity = 1)
     @total += price * quantity
     i = quantity
-    until i = 0 do 
+    until i == 0 do 
       @item << title
       i -= 1
     end
